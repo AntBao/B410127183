72c5d496e7cf4df7a6d5aa7a9e6122877ab33002
AntBao <leo83323@gmail.com>
2018-01-10 18:51:44 +0800

    first commit

---
+++ WpfApplication2/Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace WpfApplication2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (imageList1.Images.Count > 0)
+            {
+                pictureBox1.Image = imageList1.Images[0];
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (imageList1.Images.Count > 1)
+            {
+                pictureBox1.Image = imageList1.Images[1];
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (imageList1.Images.Count > 2)
+            {
+                pictureBox1.Image = imageList1.Images[2];
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (imageList1.Images.Count > 3)
+            {
+                pictureBox1.Image = imageList1.Images[3];
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            if (imageList1.Images.Count > 4)
+            {
+                pictureBox1.Image = imageList1.Images[4]; ;
+            }
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            imageList1.Images.Clear();
+        }
+    }
+}
---
+++ WpfApplication2/MainWindow.xaml.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.IO;
+
+namespace WpfApplication2
+{
+    /// <summary>
+    /// MainWindow.xaml 的互動邏輯
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click_1(object sender, RoutedEventArgs e)
+        {
+            var fullFilePath = @"C:\Users\Public\Pictures\Sample Pictures\Chrysanthemum.jpg";
+
+            FileInfo fi = new FileInfo(fullFilePath);
+
+            BitmapImage bitmap = new BitmapImage();
+            bitmap.BeginInit();
+            bitmap.UriSource = new Uri(fullFilePath, UriKind.Absolute);
+            bitmap.EndInit();
+
+            image1.Source = bitmap;
+        }
+
+        private void button2_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void button4_Click(object sender, RoutedEventArgs e)
+        {
+            
+        }
+
+        private void button6_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        int i = 1;
+        private void button5_Click(object sender, RoutedEventArgs e)
+        {
+            for (i = 1; i <= 1; i++)
+            {
+                if (i == 1)
+                {
+                    Form1 frame = new Form1();
+                    frame.Width = 800 - 10 * i;
+                    frame.Height = 600 - 10 * i;
+                    frame.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+                    frame.Text = "Windows" + i;
+                    frame.Show();
+                }
+            }
+        }
+    }
+}
